94f41664d0bf745ae2a304a3156f1d6a9de6c9fa
Jingchao Yu <jingchaoyu@hotmail.com>
2021-01-01 21:00:59 +0000

    Done.

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,11 @@
 def find_max_value(array)
-  # Add your solution here
+  index = 0 
+  highest_so_far = nil 
+  while array[index] do
+    if highest_so_far == nil || highest_so_far < array[index]
+      highest_so_far = array[index]
+    end 
+  index += 1 
+  end
+  return highest_so_far     
 end
